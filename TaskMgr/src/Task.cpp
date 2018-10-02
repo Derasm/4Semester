4c2f82d81042ebd473b184d1e26d9b07768067c3
Dennis Rasmussen <denn5682@gmail.com>
2018-10-02 02:02:01 +0200

    added now time

--- TaskMgr/src/Task.cpp
+++ TaskMgr/src/Task.cpp
@@ -1,24 +1,27 @@
 #include "User.h"
 #include "date.h" // special library from github.
+#include <chrono>
 class Task
 {
   private:
     auto currentDate;
     auto currentTime;
     Point from;
-    point to;
+    Point to;
+    int priority;
 
   public:
     Task(Point from, Point to);
     ~Task();
 };
 
-Task::Task(Point from, Point to)
+Task::Task(Point from, Point to, int priority)
 {
     from -> from;
     to -> to;
+    priority -> priority;
     currentDate = floor<days>(system_clock::now()); // current date.
-    currentTime = 
+    currentTime = system_clock::now(); // should get current time.
 }
 
 Task::~Task()
