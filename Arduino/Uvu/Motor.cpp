bd1c3b68677e3f8ca86ba17a04d65974468111a2
Dennis Rasmussen <denn5682@gmail.com>
2018-11-17 02:05:18 +0100

    added report updated code standard

--- Arduino/Uvu/Motor.cpp
+++ Arduino/Uvu/Motor.cpp
@@ -23,79 +23,90 @@ Motor::Motor(int In1pin, int In2pin, int PWMpin, int offset)
   PWM = PWMpin;
 
   offset = offset;
-  
+
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(PWM, OUTPUT);
-  
 }
 //private functions
-void Motor::fwd(int speed){
+void Motor::fwd(int speed)
+{
   digitalWrite(in1, LOW);
-  digitalWrite (in2, HIGH);
+  digitalWrite(in2, HIGH);
   analogWrite(PWM, speed);
 }
-void Motor::rev(int speed){
+void Motor::rev(int speed)
+{
   digitalWrite(in1, LOW);
-   digitalWrite(in2, HIGH);
-   analogWrite(PWM, speed);
+  digitalWrite(in2, HIGH);
+  analogWrite(PWM, speed);
 }
 
 //public functions
-void Motor::Drive(int speed){
+void Motor::Drive(int speed)
+{
   speed = speed * offset;
-  if(speed >=0) fwd(speed);
+  if (speed >= 0)
+    fwd(speed);
   else
   {
     rev(-speed);
   }
-  
 }
 
-void Motor::Drive(int speed, int duration){
-  
+void Motor::Drive(int speed, int duration)
+{
+
   speed = speed * offset;
-  if (speed>=0) fwd(speed);
-  else rev(-speed);
+  if (speed >= 0)
+    fwd(speed);
+  else
+    rev(-speed);
   //duration aint doing anything.
   delay(duration);
 }
-void Motor::Brake(){
+void Motor::Brake()
+{
   digitalWrite(in1, HIGH); //takes pin and current
   digitalWrite(in2, HIGH); //both pins set to high to facilitate stop.
-  analogWrite (PWM,0); // takes pin and speed
+  analogWrite(PWM, 0);     // takes pin and speed
 }
-void Motor::Standby(){
+void Motor::Standby()
+{
   // digitalWrite(standby, LOW); //as HIGH is necessary to drive.
 }
 
-void Motor::Forward(Motor motor1, Motor motor2){
+void Motor::Forward(Motor motor1, Motor motor2)
+{
   //takes two motors, calls forward or drive on both.
   motor1.Drive(DEFAULTSPEED);
   motor2.Drive(DEFAULTSPEED);
 }
-void Motor::Backward(Motor motor1, Motor motor2, int speed){
+void Motor::Backward(Motor motor1, Motor motor2, int speed)
+{
   int temp = abs(speed); //IF statement checks value of temp.
-	motor1.Drive(-temp);
-	motor2.Drive(-temp);
+  motor1.Drive(-temp);
+  motor2.Drive(-temp);
 }
-void Motor::Left(Motor left, Motor right, int speed){
-  int temp = abs(speed)/2;
+void Motor::Left(Motor left, Motor right, int speed)
+{
+  int temp = abs(speed) / 2;
   left.Drive(-temp);
   right.Drive(temp);
 }
-void Motor::Right(Motor left, Motor right, int speed){
-  int temp = abs(speed)/2;
+void Motor::Right(Motor left, Motor right, int speed)
+{
+  int temp = abs(speed) / 2;
   left.Drive(temp);
   right.Drive(-temp);
 }
-void Brake(Motor motor1, Motor motor2){
+void Brake(Motor motor1, Motor motor2)
+{
   motor1.Brake();
   motor2.Brake();
 }
 //Will be implemented later, when app is created for phone
-void Motor::Drive(Direction direction){
+void Motor::Drive(Direction direction)
+{
   //TODO
 }
-
-
