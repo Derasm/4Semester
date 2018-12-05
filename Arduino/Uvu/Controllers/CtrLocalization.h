10076128cdcdec2343b0c20e2eec8ca1e4b7599d
Dennis Rasmussen <denn5682@gmail.com>
2018-12-05 21:39:45 +0100

    updated program

---
+++ Arduino/Uvu/Controllers/CtrLocalization.h
@@ -0,0 +1,15 @@
+#pragma once
+#define WIDTH 10
+#define HEIGHT 10
+/*
+har ansvar for gridmappet og opdatering af bottens placering. 
+Dette gøres med to lister, en not-visited list of nodes, og en visited list of nodes.
+*/
+class CtrLocalization
+{
+private:
+    
+public:
+    CtrLocalization(/* args */);
+    ~CtrLocalization();
+};
