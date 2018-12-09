a917467933e6ecbdaece037b8e0e9fd54889df8d
Dennis Rasmussen <denn5682@gmail.com>
2018-12-09 22:37:43 +0100

    updated report and uvu

---
+++ Arduino/Uvu/Controllers/CtrDistance.h
@@ -0,0 +1,18 @@
+
+class CtrDistance
+{
+private:
+    /* data */
+public:
+    CtrDistance(/* args */);
+    ~CtrDistance();
+    int GetDistanceDriven();
+};
+
+CtrDistance::CtrDistance(/* args */)
+{
+}
+
+CtrDistance::~CtrDistance()
+{
+}
--- Arduino/Uvu/Uvu.ino
+++ Arduino/Uvu/Uvu.ino
@@ -1,5 +1,6 @@
 #include "arduino.h"
 #include "CtrMotor.h" // controller of the motors
+#include "CtrDistance.h" // accelerometer.
 
 //Pins for the shield. 
 #define PWMA 3 
@@ -17,9 +18,24 @@ void setup(){
     //setup things before the program can run.
     
 }
-void loop(){
-    /*
+/*
     will run in a positive feedback loop where a task is sent to the bot, the bot figures a route and moves to the area.
-
     */
+void loop(){
+    Motor leftMotor = ctrMotor.CreateMotor(8,10,9,0);
+    Motor rightMotor = ctrMotor.CreateMotor(12,13,3,0);
+    CtrMotor ctrMotor = CtrMotor();
+    
+   int gridDistance = 10;
+   CtrDistance distance = CtrDistance();
+   
+   for(int i = 0; i < ListOfActions.size(); i++)
+   {
+       while(distance.GetDistanceDriven() <= gridDistance){
+       //cases for the different actions, until a better solution is found. 
+    }
+   }
+
+//end of method
 }
+
